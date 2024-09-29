b63d453039264a254f70ff310b2020961186e439
Abd Al-rheem Yaseen <112316026+abdar7eem@users.noreply.github.com>
2024-09-29 10:51:05 +0300

    Create Problem-281A-Word Capitalization-solution.java

---
+++ Problem-281A-Word Capitalization-solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ 
+public class WordCapitalization{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String str=input.next();
+        String newStr=str.substring(0,1).toUpperCase()+str.substring(1);        
+        System.out.println(newStr);
+    }
+}
